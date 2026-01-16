70fe9afdad9afcb120483ad56d85080bd1954217
nivinuupauly20264k111-afk <nivinuupauly20264k111@gmail.com>
2026-01-16 20:08:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -74,7 +74,7 @@ STAR_PREMIUM_PLANS = {
 # ============================
 # MongoDB Configuration
 # ============================
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://aliabhatta144_db_user:Adarsh12345@cluster0.ng9wbyb.mongodb.net/?appName=Cluster0")  # MongoDB URI for the database
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://nivinpaulynivinpauly272_db_user:Adarsh12345@cluster0.j292wr2.mongodb.net/?appName=Cluster0")  # MongoDB URI for the database
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0") # Database name (default: cluster)
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'dreamcinezone_files') # Collection name (default: dreamcinezone_files)
 
