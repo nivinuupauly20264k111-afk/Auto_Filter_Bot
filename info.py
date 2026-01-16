ce3fec56ffa518f9c7fba04af24524321b741aa9
nivinuupauly20264k111-afk <nivinuupauly20264k111@gmail.com>
2026-01-16 18:38:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -80,7 +80,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'dreamcinezone_files') # Collec
 
 # If MULTIPLE_DB Is True Then Fill DATABASE_URI2 Value Else You Will Get Error.
 MULTIPLE_DB = is_enabled(os.environ.get('MULTIPLE_DB', "False"), False) # Type True For Turn On MULTIPLE DB FUNTION 
-DATABASE_URI2 = environ.get('DATABASE_URI2', "")  # MongoDB URI for the second database (if MULTIPLE_DB is True)
+DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://messiuuumessiuuu31_db_user:Adarsh12345@cluster0.vs5ille.mongodb.net/?appName=Cluster0")  # MongoDB URI for the second database (if MULTIPLE_DB is True)
 # ============================
 # Movie Notification & Update Settings
 # ============================
